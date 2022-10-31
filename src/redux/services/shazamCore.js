22deda9ce9318bb93a3a7349619173d2a1f7476c
martinChakarov1337 <martin.chakarov121@gmail.com>
2022-10-31 16:27:02 +0300

    finished Loader component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -4,14 +4,14 @@ export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-        prepareHeaders: () => {
+        prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', '49b0aa6063msh2a62dd444570fa4p1488f2jsn077347258d54');
 
             return headers;
-        }
+        },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({query: '/charts/world'}),
+        getTopCharts: builder.query({query: () => '/charts/world'}),
     }),
 });
 
