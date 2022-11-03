141ebd8bbd30fb8dd8ad1c569b89b52d8bbd7217
martinChakarov1337 <martin.chakarov121@gmail.com>
2022-11-03 16:24:43 +0200

    fixed bugs
    -fixed bug with not displaying pause icon when running a song
    - fixed bug with not properly getting paramereters from url when fetching info for artist details page

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,36 +1,34 @@
-import {useParams} from 'react-router-dom';
-import {useSelector} from 'react-redux';
-import {DetailsHeader,Error,Loader,RelatedSongs} from '../components';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 
 const ArtistDetails = () => {
-    const { id: artistId } = useParams();
-    const {activeSong, isPlaying} = useSelector((state) => state.player);
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
 
-    const {data: artistData, isFetching:isFetchingArtistDetails ,error} = useGetArtistDetailsQuery({artistId});
+  if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
 
-    console.log(Object.values(artistData?.songs))
+  if (error) return <Error />;
 
-    if (isFetchingArtistDetails){
-        <Loader title="Loading artist details" />;
-    }
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={artistId}
+        artistData={artistData}
+      />
 
-    if (error){
-        <Error />;
-    }
-
-    return (
-        <div className="flex flex-col">
-          <DetailsHeader artistId={artistId} artistData={artistData} />
-          <RelatedSongs
-           artistId={artistId}
-           data={Object.values(artistData?.songs)}
-           activeSong={activeSong}
-           isPlaying={isPlaying}
-           />
-        </div>
-    )
-}
+      <RelatedSongs
+        data={Object.values(artistData?.songs)}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
 
 export default ArtistDetails;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,15 +12,21 @@ export const shazamCoreApi = createApi({
     }),
     endpoints: (builder) => ({
         getTopCharts: builder.query({query: () => '/charts/world'}),
+        getSongsByGenre: builder.query({query: (genre) => `/charts/genre-world?genre_code=${genre}`}),
         getSongDetails: builder.query({query:({songid}) =>  `/tracks/details?track_id=${songid}`}),
         getSongsRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}`}),
-        getArtistDetails: builder.query({query: ({artistId}) => `/artists/details?artist_id=${artistId}`})
+        getArtistDetails: builder.query({query: (artistId) => `/artists/details?artist_id=${artistId}`}),
+        getSongsByCountry: builder.query({query: ({country}) => `/charts/country?country_code=${country}`}),
+        getSongsBySearch: builder.query({query: ({searchTerm}) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`}),
     }),
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
     useGetSongDetailsQuery,
     useGetSongsRelatedQuery,
-    useGetArtistDetailsQuery
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery
 } = shazamCoreApi;
